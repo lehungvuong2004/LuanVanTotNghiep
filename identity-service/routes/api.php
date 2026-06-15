93aebd094271c51c09716e3d5b57f31db7d5d1cf
lehungvuong <lehungvuong2004@gmail.com>
2026-06-15 22:22:36 +0700

    change docker

--- identity-service/routes/api.php
+++ identity-service/routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::group(['prefix' => 'auth'], function () {
+Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::post('google', [AuthController::class, 'googleLogin']);
-
+    // phải được xác định bằng token.
     Route::middleware('auth:api')->group(function () {
         Route::get('users', [AuthController::class, 'getUsers']);
     });
